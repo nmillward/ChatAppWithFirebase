55acd4e60c3a856a910b734cb247012a11da9f07
Nick Millward <nicholasmillward@gmail.com>
2016-03-05 21:36:52 -0500

    remove fab

--- app/src/main/java/com/superlifesize/chatappwithfirebase/MainActivity.java
+++ app/src/main/java/com/superlifesize/chatappwithfirebase/MainActivity.java
@@ -1,13 +1,10 @@
 package com.superlifesize.chatappwithfirebase;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.firebase.client.Firebase;
 
@@ -22,14 +19,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
