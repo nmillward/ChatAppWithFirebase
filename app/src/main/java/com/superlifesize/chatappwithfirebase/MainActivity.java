8762152400af10ec0b5578a9098004cf4dda15b8
Nick Millward <nicholasmillward@gmail.com>
2016-03-05 21:59:29 -0500

    instantiate views

--- app/src/main/java/com/superlifesize/chatappwithfirebase/MainActivity.java
+++ app/src/main/java/com/superlifesize/chatappwithfirebase/MainActivity.java
@@ -2,9 +2,12 @@ package com.superlifesize.chatappwithfirebase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.firebase.client.Firebase;
 
@@ -13,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     public static final String FIREBASE_URL = "https://chatappwithfirebase.firebaseio.com/";
 
     Firebase firebaseRootRef;
+    RecyclerView chatMessageRecyclerView;
+    EditText chatMessageEditText;
+    Button chatMessageSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
         //Reference to the Firebase database for this app
         firebaseRootRef = new Firebase(FIREBASE_URL);
 
+        chatMessageRecyclerView = (RecyclerView) findViewById(R.id.chatRecyclerView);
+        chatMessageEditText = (EditText) findViewById(R.id.chatMessageEditText);
+        chatMessageSendButton = (Button) findViewById(R.id.chatMessageSendButton);
+        
     }
 
     @Override
